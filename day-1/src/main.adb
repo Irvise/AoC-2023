aa3cd117e0d1b747f26854d2703e4b60f7324110
Fernando Oleo Blanco <irvise@irvise.xyz>
2023-12-02 18:57:44 +0100

    Completed second part of the challenge

--- day-1/src/main.adb
+++ day-1/src/main.adb
@@ -1,5 +1,7 @@
 with Ada.Text_IO; use Ada.Text_IO; -- Used to read the input
 with Ada.Strings.Bounded; -- Accumulate the string
+with Ada.Strings.Fixed; use Ada.Strings.Fixed;
+with Ada.Characters.Handling; use Ada.Characters.Handling;
 
 procedure Main is
    -- Input data
@@ -8,6 +10,7 @@ procedure Main is
 
    -- Type to parse onto
    subtype Number_Char is Character range '0' .. '9';
+   type Number_Letter is (zero, one, two, three, four, five, six, seven, eight, nine);
 
    -- String acumulator
    package B_String is new
@@ -23,13 +26,31 @@ procedure Main is
    is
       Numeric_String : Bounded_String;
    begin
+      Put_Line("Line read is: " & S);
       for I in S'Range loop
+
+         -- Check for actual numeric values
          if S(I) in Number_Char then
             Append(Numeric_String, S(I));
          end if;
+
+         -- Check for spelled numbers
+         for J in Number_Letter loop
+            -- Check that we are not trying to access over the length of the string
+            if S'Length + 1 - I >= J'Image'Length then
+               -- Put_Line("Checking substring " & S(I .. I + J'Image'Length - 1));
+               if S(I .. I + J'Image'Length - 1) = To_Lower(J'Image) then
+                  Put_Line("Checking substring " & S(I .. I + J'Image'Length - 1));
+                  Put_Line("Match found!");
+                  Put_Line("Appending" & Number_Letter'Pos(J)'Image);
+                  -- Careful!!! 'Image adds an estra whitespace!!!!!!
+                  Append(Numeric_String,
+                         Trim(Number_Letter'Pos(J)'Image, Ada.Strings.Left));
+               end if;
+            end if;
+         end loop;
       end loop;
-      Put_Line("Numeric String is:");
-      Put_Line(To_String(Numeric_String));
+      Put_Line("Numeric String is: " & To_String(Numeric_String));
       return To_String(Numeric_String);
    end;
 
